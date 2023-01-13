5050ea8a22daa78da148296292314bb4601f4d2b
Anjima Saadasivan <sadasivananjima@gmail.com>
2023-01-13 16:04:53 +0530

    Login Page Task

---
+++ hello_world/App.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './src/Home';
+import Login from './src/Login';
+import Userprofile from './src/Userprofile';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Userprofile" component={Userprofile} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
